ad67b9255a6a7b8a94375b677437ada0dd9d8f3e
winternis <nicolas.nativel@insa-lyon.fr>
2015-11-01 12:10:44 +0100

    comments on alphabeta

--- minmax.pl
+++ minmax.pl
@@ -2,37 +2,55 @@
 :-consult(move).
 :-consult(randomHeuristic).
 
-
+% maxN(+[[Eval,Position]], -[BestEval,Position])
+% Get the best couple (ordered on the Eval) from the list of couple [Eval,Position]
+%
 maxN([X],X).
 maxN([[X1|L]|Xs],[X1|L]):- maxN(Xs,[Y|_]), X1 >=Y.
 maxN([[X1|_]|Xs],[N|QN]):- maxN(Xs,[N|QN]), N > X1.
 
 
-
+% minN(+[[Eval,Position]], -[BestEval,Position])
+% Get the minimal couple (ordered on the Eval) from the list of couple [Eval,Position]
+%
 minN([X],X).
 minN([[X1|L]|Xs],[X1|L]):- minN(Xs,[Y|_]), X1 =< Y.
 minN([[X1|_]|Xs],[N|QN]):- minN(Xs,[N|QN]), N < X1.
 
-
+% minimax(+Player, +Position, -Eval, -NextPosition, +Depth)
+% Execute the minimax algorithm. NextPosition is the best move that Player can do if he thinks (Depth) moves further.
+%
+% End of recursion when Depth is 0 or when Position is a winning position.
 minimax(_,V,Eval,V,_) :- final(V,_),hRandom(V,Eval),!.
 minimax(_,V,Eval,V,Depth) :- hRandom(V,Eval), Depth == 0,!.
 
+% Minimax from the point of view of max
 minimax(w,V,Eval,VNext,Depth) :- DepthNext is Depth-1,
 	setof([TestEval,TestNext],combiMoveMinimax(V,w,b,TestNext,TestEval,DepthNext),Evals),
 	maxN(Evals,[Eval,VNext]).
 
+% Minimax from the point of view of min
 minimax(b,V,Eval,VNext,Depth) :- DepthNext is Depth-1,
 	setof([TestEval,TestNext],combiMoveMinimax(V,b,w,TestNext,TestEval,DepthNext),Evals),
 	minN(Evals,[Eval,VNext]).
 
+% combiMoveMinimax(+Position, +X, +Y, -TestNext, -TestEval, +DepthNext)
+% find every couple TestNext, TestEval, that directly follows the Position.
+% DepthNext is necessary because combiMoveMinimax needs to call minimax.
+%
 combiMoveMinimax(V,X,Y,TestNext,TestEval,DepthNext) :- move(V,X,TestNext),minimax(Y,TestNext,TestEval,_,DepthNext).
 
 %testJeu(N,LNext) :- generateGraph(N), generateVerticesEmptyList(N,L), minimax(w,L,_,LNext,2).
 testJeu(_,LNext) :- minimax(w,[b,b,e,e,w,e],_,LNext,1).
 
-play(C,P,CNext) :- minimax(P,C,_,CNext,2).
-%play(C,P,CNext) :- alphabeta(P,C,_,CNext,2, -101,101).
+%play(C,P,CNext) :- minimax(P,C,_,CNext,2).
+play(C,P,CNext) :- alphabeta(P,C,_,CNext,2, -101,101).
 
+
+
+% alphabeta((Same as minimax), +Alpha, +Beta)
+% is a minimax with alpha-beta pruning. When launched, alpha is -infinite and beta is +infinite (every lower bound of the heuristic for alpha, and upper bound for beta works)
+%
 alphabeta(_,V,Eval,_,_,_,_) :- final(V,_),hRandom(V,Eval),!.
 alphabeta(_,V,Eval,V,Depth,_,_) :- hRandom(V,Eval), Depth == 0,!.
 
@@ -44,40 +62,61 @@ alphabeta(b,V,Eval,VNext,Depth, Alpha, Beta) :- DepthNext is Depth-1,
 	find(V, Evals, b, w, DepthNext, Alpha, Beta),
 	minN(Evals,[Eval,VNext]).
 
+% combiMoveAlphabeta(+V, +ListToAvoid, +X, +Y, -TestNext, -TestEval, +DepthNext, +Alpha, +Beta)
+% is the equivalent of combiMoveMinimax. Because of the implementation of find, it needs a parameter ListToAvoid
+% which is the list of the couples (TestEval,TestNext) already found.
+%
 combiMoveAlphabeta(V,ListToAvoid,X,Y,TestNext,TestEval,DepthNext,Alpha, Beta) :- move(V,X,TestNext),not(member([_,TestNext],ListToAvoid)),alphabeta(Y,TestNext,TestEval,_,DepthNext, Alpha, Beta).
 
 
+
+% find(+V, -Results, +Player, +Opponent, +Depth, +Alpha, +Beta)
+% is an equivalent of setof, but can stop if necessary (in case of alpha or beta pruning).
+%
+% find(+V, +Accumulator, -Results, +Player, +Opponent, +Depth, +Alpha, +Beta)
+% is the predicate that build Results, with the help of an Accumulator.
+% 
 find(V, Res, Player, Opponent, Depth, Alpha, Beta) :- find(V, [], Res, Player, Opponent, Depth, Alpha, Beta), !.
 find(V, Acc, Res, Player, Opponent, Depth, Alpha, Beta) :-
 	combiMoveAlphabeta(V, Acc, Player, Opponent,TestNext,TestEval,Depth,Alpha, Beta),!,
-	(pruneAlpha(TestEval,Alpha,Player), % If min plays, he can eventually do an alpha prune.
-	pruneBeta(TestEval, Beta, Player)) -> ( % If max plays, he can eventually do a beta prune.
+	(pruneAlpha(TestEval,Alpha,Player),                           % If min plays, he can eventually do an alpha prune.
+	pruneBeta(TestEval, Beta, Player)) -> (                       % If max plays, he can eventually do a beta prune.
 		uList([TestEval,TestNext], Acc, AccNew),
-		updateBeta(Beta,TestEval,Player,NewBeta), % If min plays, he updates beta.
-		updateAlpha(Alpha,TestEval,Player,NewAlpha), % If max plays, he updates alpha.
+		updateBeta(Beta,TestEval,Player,NewBeta),                 % If min plays, he updates beta.
+		updateAlpha(Alpha,TestEval,Player,NewAlpha),              % If max plays, he updates alpha.
 		find(V, AccNew, Res, Player, Opponent, Depth, NewAlpha, NewBeta)).
 
+% End of the recursion when Acc and Res are the same list.
 find(_, Acc, Acc, _, _,_,_,_).
 
+
+
+% uList(+Elem, +Acc, -AccNew)
+% Add Elem to Acc if it is not already member. The Acc is returned in AccNew. 
+%
 uList(X, [], [X])  :- !.
 uList(H, [H|_], _) :- !, fail.
 uList(X, [H|T], L) :- uList(X, T, Rtn), L = [H|Rtn].
 
-%If min plays, he updates only beta. Max updates alpha.
-%Pruning only happens on beta for max, and on alpha for min.
+% If min plays, he updates only beta. Max updates alpha.
+% Pruning only happens on beta for max, and on alpha for min.
 
-%update alpha as the max between alpha and the current value.
-%updateAlpha(+Alpha, +Val, +Player, -NewAlpha)
+% updateAlpha(+Alpha, +Val, +Player, -NewAlpha)
+% update alpha as the max between alpha and the current value.
+%
 updateAlpha(Alpha, _, Player, NewAlpha) :- Player == 'b', NewAlpha is Alpha.
 updateAlpha(Alpha, Val, Player, NewAlpha) :- Player == 'w', NewAlpha is max(Alpha,Val).
 
-%update beta as the min between beta and the current value.
-%updateBeta(+Beta, +Val, +Player, -NewBeta)
+
+% updateBeta(+Beta, +Val, +Player, -NewBeta)
+% update beta as the min between beta and the current value.
+%
 updateBeta(Beta, _, Player, NewBeta) :-Player == 'w', NewBeta is Beta.
 updateBeta(Beta, Val, Player, NewBeta) :- Player == 'b', NewBeta is min(Beta,Val).
 
-%Pruning condition. Happens if pruneAlphaBeta is evaluate to false.
-%prune(+Val,+AlphaBeta, +Player)
+% prune(+Val,+AlphaBeta, +Player)
+% Pruning condition. Pruning needs to happen if pruneAlphaBeta is evaluate to false.
+%
 pruneAlpha(_,_,'w').  %Never pruneAlpha when player is white.
 pruneAlpha(Val,Alpha, Player) :- Player=='b', Val > Alpha.
 
