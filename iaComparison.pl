05b1dd83a0513234a456819d50a2df2dfc00d968
TFavrot <thomas.favrot@insa-lyon.fr>
2015-11-03 11:44:48 +0100

    iaComparison has recStats, which launches N matches

--- iaComparison.pl
+++ iaComparison.pl
@@ -11,17 +11,26 @@ generateEmptyPosition(N, L) :- B is N*(1+N)/2, recList(B,L).
 %
 % Launch a match IA1 vs IA2 (IA should be the name of the heuristic)
 %
-match(IA1, IA2, Size, Winner) :- 	generateGraph(Size), 
-									generateEmptyPosition(Size, List),
-									match(IA1, IA2, List, w, Winner).
+match(IA1, IA2, Size, Depth, Winner) :- 	generateGraph(Size), 
+										generateEmptyPosition(Size, List),
+										match(IA1, IA2, Depth, List, w, Winner).
 							
-match(IA1, IA2, Position, w, Winner) :- play(Position, w, NewPosition, IA1), 
-										((win(NewPosition, w) -> Winner is 1,!) ;
-										(match(IA1, IA2, NewPosition, b, Winner))).
+match(IA1, IA2, Depth, Position, w, Winner) :- 	play(Position, w, NewPosition, IA1, Depth), 
+												((win(NewPosition, w) -> Winner is 1,!) ;
+												(match(IA1, IA2, Depth, NewPosition, b, Winner))).
 
-match(IA1, IA2, Position, b, Winner) :- play(Position, b, NewPosition, IA2),
-										(win(NewPosition, b) -> Winner is 2,! ;
-										match(IA1, IA2, NewPosition, w, Winner)).
+match(IA1, IA2, Depth, Position, b, Winner) :-	play(Position, b, NewPosition, IA2, Depth),
+												(win(NewPosition, b) -> Winner is 2,! ;
+												match(IA1, IA2, Depth, NewPosition, w, Winner)).
 
 
+recStats(0,Wwins, Bwins,_,_,_,_,Wwins,Bwins) :- !.
+recStats(N, Wwins, Bwins, IA1, IA2, Size, Depth, Wfinal, Bfinal) :- 
+		N1 is N-1, 
+		match(IA1, IA2, Size, Depth, Winner),
+		
+		((Winner is 1 -> ((NewWwins is Wwins+1),(NewBwins is Bwins)));
+						(NewBwins is Bwins+1),(NewWwins is Wwins)),
+		
+		recStats(N1, NewWwins, NewBwins, IA1, IA2, Size, Depth, Wfinal, Bfinal).
 
